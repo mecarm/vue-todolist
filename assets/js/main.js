7793cbc71f7b091425bfa4072cac5603c51c631f
mecarm <melecarmine99@gmail.com>
2022-11-14 15:15:01 +0100

    Create task dinamiche

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,26 @@
 var app = new Vue({
     el: '#app',
     data: {
-
+        todoList:[ 
+            {
+                testo: 'Imparare HTML'
+            },
+            {
+                testo: 'Imparare CSS'
+            },
+            {
+                testo: 'Imparare JavaScript'
+            },
+            {
+                testo: 'Imparare Vue.js'
+            },
+            {
+                testo: 'Imparare PHP & MYSQL'
+            },
+            {
+                testo: 'Imparare Laravel'
+            }
+        ]
     },
     methods: {
 
